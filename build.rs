d491e80bc9f49c08f145accc7fa482a67e1193c9
decaday <myDecaday@outlook.com>
2024-07-19 09:17:53 +0800

    fix and add wapper.h

--- build.rs
+++ build.rs
@@ -7,11 +7,11 @@ fn main() {
     #[cfg(feature = "recompile")]
     compile();
 
-    #[cfg(not(feature = "regenerate-bindings"))]
+    #[cfg(feature = "regenerate-bindings")]
     generate_bindings();
 }
 
-fn recompile(){
+fn compile(){
     // Configure cross compilation
     let target = env::var("TARGET").unwrap();
     if !target.starts_with("thumbv6m-none-eabi") {
@@ -19,32 +19,42 @@ fn recompile(){
     }
 
     // Configure Compiler
-    // let clang = "arm-clang";
-    let cflags = "-mcpu=cortex-m0plus -mthumb -mthumb-interwork -ffunction-sections -fdata-sections -fno-common -fmessage-length=0";
-    let debug_flags = match &*env::var("PROFILE").unwrap() {
-        "release" => "-Ofast",
-        "debug" => "-Og -g",
-        "relwithdebinfo" => "-Ofast -g",
-        "minsizerel" => "-Os",
-        _ => "-Og -g",
+    let mut opt_level = "s";
+    let mut debug_flag = None;
+    match &*env::var("PROFILE").unwrap() {
+        "release" => opt_level = "fast",
+        "debug" => { opt_level = "g"; debug_flag = Some("-g") },
+        "relwithdebinfo" => { opt_level = "fast"; debug_flag = Some("-g") },
+        "minsizerel" => opt_level = "s",
+        _ => { opt_level = "g"; debug_flag = Some("-g") },
     };
 
     let mut build = Build::new();
-    build//.compiler(clang)
-        .flag(cflags)
-        .flag(debug_flags)
+    build.compiler("arm-none-eabi-gcc")
+        .opt_level_str(opt_level)
+        .flag("-mcpu=cortex-m0plus")
+        .flag("-mthumb-interwork")
+        .flag("-ffunction-sections")
+        .flag("-fdata-sections")
+        .flag("-fno-common")
+        .flag("-fmessage-length=0")
+        //.flag(debug_flags)
         .files(vec![
-            "Drivers/CMSIS/Device/PY32F0xx/Source/system_py32f0xx.c",
-            "Src/extra_functions.c",
+            "PY32F0_Drivers/CMSIS/Device/PY32F0xx/Source/system_py32f0xx.c",
+            "csrc/src/extra_functions.c",
         ])
-        .include("Inc")
-        .include("Drivers/CMSIS/Include")
-        .include("Drivers/CMSIS/Device/PY32F0xx/Include")
-        .include("Drivers/PY32F0xx_HAL_Driver/Inc")
+        .include("csrc/inc")
+        .include("PY32F0_Drivers/CMSIS/Include")
+        .include("PY32F0_Drivers/CMSIS/Device/PY32F0xx/Include")
+        .include("PY32F0_Drivers/PY32F0xx_HAL_Driver/Inc")
         .define("DEBUG", None)
         .define("PY32F030x6", None);
 
-    for entry in std::fs::read_dir("Drivers/PY32F0xx_HAL_Driver/Src").unwrap() {
+    if let Some(debug_flag_str) = debug_flag {
+        build.flag(debug_flag_str);
+    }
+
+    for entry in std::fs::read_dir("PY32F0_Drivers/PY32F0xx_HAL_Driver/Src").unwrap() {
         let path = entry.unwrap().path();
         if path.extension().map_or(false, |ext| ext == "c") {
             build.file(path);
@@ -58,11 +68,12 @@ fn generate_bindings(){
 
     // binding
     let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .clang_arg("-IInc")
-        .clang_arg("-IDrivers/CMSIS/Include")
-        .clang_arg("-IDrivers/CMSIS/Device/PY32F0xx/Include")
-        .clang_arg("-IDrivers/PY32F0xx_HAL_Driver/Inc")
+        .header("csrc/inc/wrapper.h")
+        .clang_arg("--target=arm-none-eabi")
+        .clang_arg("-Icsrc/inc")
+        .clang_arg("-IPY32F0_Drivers/CMSIS/Include")
+        .clang_arg("-IPY32F0_Drivers/CMSIS/Device/PY32F0xx/Include")
+        .clang_arg("-IPY32F0_Drivers/PY32F0xx_HAL_Driver/Inc")
         .generate()
         .expect("Unable to generate bindings");
 
---
+++ csrc/inc/wrapper.h
@@ -0,0 +1,7 @@
+#ifndef PY32CSDK_HAL_SYS_WRAPPER_H
+#define PY32CSDK_HAL_SYS_WRAPPER_H
+
+#include "py32f0xx_hal.h"
+#include "extra_functions.h"
+
+#endif //PY32CSDK_HAL_SYS_WRAPPER_H
